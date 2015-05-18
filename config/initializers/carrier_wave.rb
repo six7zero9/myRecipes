30ad18ac83a17bfe5a39899b619d356d23b0caff
mj <michaelpauljackson@gmail.com>
2015-05-18 23:16:17 +0000

    fixed carrierwave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  Carrierwave.configure do |config|
+  CarrierWave.configure do |config|
     config.fog.credentials = {
       :provider => 'AWS',
       :aws_access_key => ENV['S3_ACCESS_KEY'],
